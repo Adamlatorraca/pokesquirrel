799fde919cd4039e4612808e1db418bf092f0f45
Adamlatorraca <adam.latorraca@gmail.com>
2020-06-22 14:03:17 -0400

    add squirrel index route

--- app/controllers/squirrel_controller.rb
+++ app/controllers/squirrel_controller.rb
@@ -1,5 +1,7 @@
 class SquirrelController < ApplicationController
     get '/squirrels' do
+        redirect_if_not_logged_in
+        @squirrels = Squirrel.all
         erb :'squirrels/index'
     end
 end
