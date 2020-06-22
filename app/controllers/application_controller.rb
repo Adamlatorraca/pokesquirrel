66ba25a9b873d42632924fbbf9e479dffc2eb671
Adamlatorraca <adam.latorraca@gmail.com>
2020-06-22 13:43:03 -0400

    add helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,20 @@ class ApplicationController < Sinatra::Base
   get "/" do
     erb :index
   end
+
+  helpers do
+    def logged_in?
+        !!session[:user_id]
+    end
+
+    def current_user  #memoization
+        @current_user ||= User.find_by_id(session[:user_id])
+
+        # if @current_user
+        #     @current_user
+        # else
+        #     @current_user = User.find_by_id(session[:user_id])
+        # end
+    end
+  end
 end
