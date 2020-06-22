37f98ae65692137e60fd9fff71edb4cb2464dc86
Adamlatorraca <adam.latorraca@gmail.com>
2020-06-22 12:45:20 -0400

    add sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,7 @@ class UsersController < ApplicationController
     configure do
         set :public_folder, 'public'
         set :views, 'app/views'
+        enable :sessions
+        set :session_secret, "fillwithdotenv"
     end
 end
