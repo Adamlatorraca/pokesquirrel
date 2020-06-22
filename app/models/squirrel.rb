28aa64c6ff856c51e3cc0b153a70d17a680be271
Adamlatorraca <adam.latorraca@gmail.com>
2020-06-22 19:15:01 -0400

    add helper

--- app/models/squirrel.rb
+++ app/models/squirrel.rb
@@ -1,3 +1,7 @@
 class Squirrel < ActiveRecord::Base
     belongs_to :user
+
+    def self.valid_params?(params)
+        return !params[:name].empty? && !params[:fur_color].empty? && !params[:mood].empty?
+    end
 end
