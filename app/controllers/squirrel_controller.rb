f013b29147b1ffb53d14849817bd51dce392fb62
Adamlatorraca <adam.latorraca@gmail.com>
2020-06-22 18:57:23 -0400

    add edit and id routes

--- app/controllers/squirrel_controller.rb
+++ app/controllers/squirrel_controller.rb
@@ -4,4 +4,31 @@ class SquirrelController < ApplicationController
         @squirrels = Squirrel.all
         erb :'squirrels/index'
     end
+
+    get '/squirrels/new' do
+        redirect_if_not_logged_in
+        erb :'squirrels/new'
+    end
+
+    get "/squirrels/:id" do
+        redirect_if_not_logged_in
+        @squirrel = Squirrel.find(params[:id])
+        erb :'squirrel/show'
+    end
+
+    get '/squirrels/:id/edit' do
+        redirect_if_not_logged_in
+        @squirrel = Squirrel.find(params[:id])
+        erb :'squirrels/edit'
+    end
+
+    post '/squirrels' do
+        redirect_if_not_logged_in
+        @squirrel = Squirrel.new(create)
+        if @squirrel.valid_params?(params)
+            redirect to '/squirrels/new'
+        end
+        @squirrel.save
+        redirect "/squirrels/#{@squirrel.id"
+    end
 end
