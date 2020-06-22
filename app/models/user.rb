fe0e857a8b679a257f497aefa59ff4df35de20fa
Adamlatorraca <adam.latorraca@gmail.com>
2020-06-22 13:34:17 -0400

    add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
+    has_secure_password
+    validates :username, presence: true
+    validates :email, presence: true
+    validates :password, presence: true
+    validates :username, uniqueness: true
+    validates :email, uniquenss: true
     has_many :squirrels
 end
