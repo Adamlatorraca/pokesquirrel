5a69392a627d1c9e2510dc1802453c3505f57b89
Adamlatorraca <adam.latorraca@gmail.com>
2020-06-22 20:03:56 -0400

    add redirect for profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
         user = User.new(params[:user])
         if user.save
             session[:user_id] = user.id
-            redirect to '/users/:id'
+            redirect to '/'
         else
             erb :'users/signup'
         end
