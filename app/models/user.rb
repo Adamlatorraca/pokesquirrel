88b5d8791d529b3661c2e2db76b5bef624dad7cd
Adamlatorraca <adam.latorraca@gmail.com>
2020-06-26 12:59:22 -0400

    remove redundant code

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ActiveRecord::Base
     has_secure_password
     validates :username, presence: true
     validates :email, presence: true
-    validates :password, presence: true
     validates :username, uniqueness: true
     validates :email, uniqueness: true
     has_many :squirrels
