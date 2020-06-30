53e0a657dbeb06750abda3d5bfd51186f58c7aad
Adamlatorraca <adam.latorraca@gmail.com>
2020-06-30 19:11:00 -0400

    remove logged in private method

--- app/controllers/squirrel_controller.rb
+++ app/controllers/squirrel_controller.rb
@@ -38,10 +38,6 @@ class SquirrelController < ApplicationController
     end
 
     private
-
-    def logged_in?
-        !!session[:user_id]
-      end
   
       def current_user
           @current_user ||= User.find_by_id(session[:user_id])
