5df3a9f86cc1dd0d40a423bfc76aaffe7126d15b
Adamlatorraca <adam.latorraca@gmail.com>
2020-06-30 19:05:06 -0400

    move helper methods to private methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,22 +20,6 @@ class ApplicationController < Sinatra::Base
   end
 
   error ActiveRecord::RecordNotFound do
-    redirect to '/'
-  end
-
-  helpers do
-    def logged_in?
-        !!session[:user_id]
-    end
-
-    def current_user
-        @current_user ||= User.find_by_id(session[:user_id])
-    end
-
-    def redirect_if_not_logged_in
-      if !logged_in?
-        redirect to '/login'
-      end
-    end
+    redirect to '/squirrels'
   end
 end
