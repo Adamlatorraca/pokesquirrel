c002a260d3c088c73f270177d60350eb35383229
Adamlatorraca <adam.latorraca@gmail.com>
2020-06-30 20:01:17 -0400

    add crud actions to route

--- app/controllers/squirrel_controller.rb
+++ app/controllers/squirrel_controller.rb
@@ -1,7 +1,7 @@
 class SquirrelController < ApplicationController
     get '/squirrels' do
         redirect_if_not_logged_in
-        all_squirrels
+        @squirrels = Squirrel.all.includes(:user)
         erb :'squirrels/index'
     end
 
@@ -21,28 +21,25 @@ class SquirrelController < ApplicationController
     end
 
     post '/squirrels' do
-        create_squirrel
+        redirect_if_not_logged_in
+        current_user.squirrels.create(params[:squirrel])
         redirect to '/squirrels'
     end
 
     patch '/squirrels/:id' do
         find_or_redirect_if_not_authorized
-        update_squirrel
+        @squirrel.update(params[:squirrel])
         redirect to "/squirrels/#{@squirrel.id}"
     end
 
     delete '/squirrels/:id' do
         find_or_redirect_if_not_authorized
-        delete_squirrel
+        @squirrel.destroy
         redirect to '/squirrels'
     end
 
     private
-  
-      def current_user
-          @current_user ||= User.find_by_id(session[:user_id])
-      end
-  
+
       def redirect_if_not_logged_in
         if !logged_in?
           redirect to '/login'
@@ -71,21 +68,5 @@ class SquirrelController < ApplicationController
       def find_or_redirect_if_not_authorized
         find_squirrel
         redirect_if_not_authorized
-      end
-
-      def all_squirrels
-        @squirrels = Squirrel.all.includes(:user)
-      end
-
-      def create_squirrel
-        current_user.squirrels.create(params[:squirrel])
-      end
-
-      def update_squirrel
-        @squirrel.update(params[:squirrel])
-      end
-
-      def delete_squirrel
-        @squirrel.destroy
       end
 end
