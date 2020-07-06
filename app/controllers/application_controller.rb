7261b3ee2d9b681edee3093184da88fd60400f92
Adamlatorraca <adam.latorraca@gmail.com>
2020-07-06 11:19:55 -0400

    add redirect helper back into app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,5 +27,11 @@ class ApplicationController < Sinatra::Base
     def current_user
       @current_user ||= User.find_by_id(session[:user_id])
     end
+
+    def redirect_if_not_logged_in
+      if !logged_in?
+        redirect to '/login'
+      end
+    end
   end
 end
